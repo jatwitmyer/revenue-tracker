9a3576d5e6c997f2ee9d91ecab513dcbff46b171
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-11-01 16:13:07 -0400

    adds index route

--- server/app.py
+++ server/app.py
@@ -2,6 +2,10 @@ from config import app
 from routes.store_routes import stores, store_by_id
 from routes.product_routes import products, products_by_id
 
+@app.route('/')
+def home():
+    return ''
+
 # run python app.py
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
