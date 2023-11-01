c4290f8d92eb3ca665400f01f801e6e672844f58
Cooper <55swimmer55@gmail.com>
2023-11-01 16:07:47 -0400

    add products route to main

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
+from config import app
 from routes.store_routes import stores, store_by_id
+from routes.product_routes import products, products_by_id
 
 # run python app.py
 if __name__ == '__main__':
---
+++ server/routes/product_routes.py
@@ -0,0 +1,91 @@
+from flask import Flask, make_response, request
+from models import db, User, Company, Store, Product, Sale, InventoryItem
+from config import app
+
+@app.route('/products', methods = ['GET', 'POST'])
+def products():
+    
+    if request.method == 'GET':
+        products = Product.query.all()
+        product_dict = [product.to_dict(rules = ('-sale', '-inventory_item')) for product in products]
+
+        response = make_response(
+            product_dict,
+            200
+        )
+
+    elif request.method == 'POST':
+        form_data = request.get_json()
+
+        try:
+            new_product_obj = Product(
+                name = form_data['name'],
+                manufacturing_cost = form_data['manufacturing_cost'] #,
+                # company_id = form_data['company_id']
+            )
+
+            db.session.add(new_product_obj)
+            db.session.commit()
+
+            response = make_response(
+                new_product_obj.to_dict(),
+                201
+            )
+        except ValueError:
+            response = make_response(
+                {"errors": ["validation errors in POST to products"]},
+                400
+            )
+            return response
+
+    return response
+
+@app.route('/products/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
+def products_by_id(id):
+    product = Product.query.filter(Product.id == id).first()
+
+    if product:
+        if request.method == 'GET':
+
+            response = make_response(
+                product.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            form_data = request.get_json()
+
+            try:
+                for attr in form_data:
+                    setattr(product, attr, form_data.get(attr))
+                
+                db.session.commit()
+
+                response = make_response(
+                    product.to_dict(rules = ('-sale', '-inventory_item')),
+                    202
+                )
+
+            except ValueError:
+                response = make_response(
+                    {"errors": ["validation errors in PATCH to products id"]},
+                    400
+                )
+                return response
+            
+        elif request.method == 'DELETE':
+            db.session.delete(product)
+            db.session.commit()
+
+            response = make_response(
+                {},
+                204
+            )
+    
+    else:
+        response = make_response(
+            {"error": "Product not found"},
+            404
+        )
+
+    return response
