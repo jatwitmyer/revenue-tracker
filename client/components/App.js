53b08c38ae257578669e23f3825a15f4c0234546
Cooper <55swimmer55@gmail.com>
2023-11-06 15:00:34 -0500

    Add basic React infrastructure

---
+++ client/components/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <div className="App">
+        </div>
+    );
+}
+
+export default App;
