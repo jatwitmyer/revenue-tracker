b49b179e69fb086376560e0b4d6db1b9be05ac8f
keyla <keyyleiva@gmail.com>
2023-11-07 17:20:40 -0500

    added classnames

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -2,11 +2,9 @@ import React, { useEffect, useState } from "react";
 
 function Stores({storesArray}) {
     return (
-        <div>
-            <li>
-                <h2>testing</h2>
-                <p>test formatting</p>
-            </li>
+        <div class="info-box" >
+                <h2 class="store-name" >testing</h2>
+                <p class="store-location" >test formatting</p>
         </div>
     )
 }
