71145f83adbd78cb5a5506be13af6d6963b2cf1e
keyla <keyyleiva@gmail.com>
2023-11-07 15:25:13 -0500

    refactored file structure

--- client/components/Stores.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from "react";
-
-function Stores({storesArray}) {
-    return (
-        <></>
-    )
-}
-
-export default Stores 
---
+++ client/src/components/Stores.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+function Stores({storesArray}) {
+    return (
+        <div>
+            <li>
+                <h2>testing</h2>
+                <p>test formatting</p>
+            </li>
+        </div>
+    )
+}
+
+export default Stores 
