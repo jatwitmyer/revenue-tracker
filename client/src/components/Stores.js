e068447d85ee237b6fa003f3d1938c5268d9c0c4
keyla <keyyleiva@gmail.com>
2023-11-07 17:55:53 -0500

    added sample store box

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 function Stores({storesArray}) {
-    console.log(storesArray.address)
+    
     return (
         <div className="info-box" >
-                <h2 className="store-name" >testing</h2>
-                <p className="store-location" >test formatting</p>
+                <h2 className="store-name" >{storesArray[0].name}</h2>
+                <p className="store-location" >{storesArray[0].address}</p>
         </div>
     )
 }
