8140d99ffbccc633f2cf82c586354e47190b91ee
keyla <keyyleiva@gmail.com>
2023-11-07 17:44:53 -0500

    added manifest

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 function Stores({storesArray}) {
+    console.log(storesArray.address)
     return (
-        <div class="info-box" >
-                <h2 class="store-name" >testing</h2>
-                <p class="store-location" >test formatting</p>
+        <div className="info-box" >
+                <h2 className="store-name" >testing</h2>
+                <p className="store-location" >test formatting</p>
         </div>
     )
 }
