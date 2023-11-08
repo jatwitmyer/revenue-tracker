32f5ef3337dc774735c3e5ad326dd5d0fcc7265c
keyla <keyyleiva@gmail.com>
2023-11-08 10:28:39 -0500

    added css

--- client/src/components/Stores.js
+++ client/src/components/Stores.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 
 function Stores({storesArray}) {
     
+    const storeName = storesArray.filter((store) => {
+        console.log(store)
+    })
+
     return (
         <div className="info-box" >
-                <h2 className="store-name" >{storesArray[0].name}</h2>
+                <h2 className="store-name" >{storeName}</h2>
                 <p className="store-location" >{storesArray[0].address}</p>
         </div>
     )
