0d7a86d94aee49f8908d156b06f79e3303320275
Jessica Twitmyer <jatwitmyer@gmail.com>
2024-01-06 21:19:20 -0500

    nav bar styling with broken links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,16 +4,14 @@ import { NavLink } from 'react-router-dom';
 function NavBar() {
 
     return (
-        <>  
-            <div class="header">
-                <h1>Revenue Tracker</h1>
-            </div>
-            <div className = "topnav">
-                <NavLink to="/sales">Sales Overview</NavLink>
-                <NavLink to="/stores">Store Details</NavLink>
-                <NavLink to="/products">Product Details</NavLink>
-            </div>
-        </>
+        <div className = "topnav">
+            <img src="SeekPng.com_dollar-sign-png_4035489.png" alt="" title=""/>
+            {/* <i class="fa fa-usd" aria-hidden="true"></i> */}
+            <NavLink className="left-nav" to="/overview">Overview</NavLink>
+            <p>Revenue Tracker</p>
+            <a className="right-nav" href="/">Log Out</a>
+            <NavLink className="right-nav" to="/account">Account</NavLink>
+        </div>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './new.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
